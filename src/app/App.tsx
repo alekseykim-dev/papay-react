11ca762cfc24ac282f2caabd74d9ba88c8cd6ce3
ALEKSEY0305 <alex.romanovich.ak@gmail.com>
2023-02-18 18:02:49 +0900

    fix: papay project standarts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import '../css/App.css';
-import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { RippleBadge } from './MaterialTheme/styled';
+import React from "react";
+import "../css/App.css";
+import { Box, Container, Typography, Stack, Button } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Stack flexDirection={"column"}>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component={"h1"} gutterBottom>
-            Create React App on Typescript with REDUX
-          </Typography>
-        </Box>
-        <Box>
-          <RippleBadge badgeContent={4}>
-            <Button color="secondary" variant="contained">
-              Contained
-            </Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
+    <Container>
+
     </Container>
   );
 }
