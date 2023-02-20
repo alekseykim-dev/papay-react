a6a17ce88575d7b41e717724170aadc71a49566e
ALEKSEY0305 <alex.romanovich.ak@gmail.com>
2023-02-21 04:39:33 +0900

    feat: develop header navbarHome component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "../css/App.css";
+import "../css/navbar.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { RestaurantPage } from "./screens/RestaurantPage";
 import { CommunityPage } from "./screens/CommunityPage";
@@ -9,13 +10,25 @@ import { MemberPage } from "./screens/MemberPage";
 import { HelpPage } from "./screens/HelpPage";
 import { LoginPage } from "./screens/LoginPage";
 import { HomePage } from "./screens/HomePage";
+import { NavbarHome } from "./components/header";
+import { NavbarRestaurant } from "./components/header/restaurant";
+import { NavbarOthers } from "./components/header/others";
 
 
 function App() {
+  const [path, setPath] = useState()
+  const main_path = window.location.pathname;
+
   return (
     <Router>
-      <div>
-        <nav>
+      {main_path == "/" ? (
+        <NavbarHome setPath={setPath} />
+      ) : main_path.includes("/restaurant") ? (
+        <NavbarRestaurant setPath={setPath} />
+      ) : (
+        <NavbarOthers setPath={setPath}/>
+      )}
+      {/* <nav>
           <ul>
             <li>
               <Link to="/restaurant">RestaurantPage</Link>
@@ -39,34 +52,33 @@ function App() {
               <Link to="/">Homepage</Link>
             </li>
           </ul>
-        </nav>
+        </nav> */}
 
-        {/* A <Switch> looks through its children <Route>s and
+      {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/restaurant">
-            <RestaurantPage />
-          </Route>
-          <Route path="/community">
-            <CommunityPage />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <MemberPage />
-          </Route>
-          <Route path="/help">
-            <HelpPage />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/restaurant">
+          <RestaurantPage />
+        </Route>
+        <Route path="/community">
+          <CommunityPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <MemberPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Stack,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+export function NavbarHome(props: any) {
+  return (
+    <div className="format home_navbar">
+      <Container>
+        <Stack
+          flexDirection={"row"}
+          className="navbar_config"
+          justifyContent={"space-between"}
+        >
+          <Box>
+            <img src="/icons/papay.svg" alt="logo" />
+          </Box>
+          <Stack
+            flexDirection={"row"}
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+            className="navbar_links"
+          >
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/" activeClassName="underline">
+                Home
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/restaurant" activeClassName="underline">
+                Restaurant
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/orders" activeClassName="underline">
+                Orders
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/community" activeClassName="underline">
+                Community
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/help" activeClassName="underline">
+                Help
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <IconButton
+                aria-label="cart"
+                id="basic-button"
+                aria-controls={undefined}
+                aria-aria-haspopup="true"
+                aria-expanded={undefined}
+                //onClick={handleClick}
+              >
+                <Badge badgeContent={3} color="secondary">
+                  <img src="/icons/shopping_cart.svg" />
+                </Badge>
+              </IconButton>
+            </Box>
+            <Box>
+              <Button
+                variant="contained"
+                style={{ color: "#ffffff", background: "#1976d2" }}
+              >
+                Log in
+              </Button>
+            </Box>
+          </Stack>
+        </Stack>
+
+        <Stack className="head_information" justifyContent={"row"}>
+          <Stack
+            justifyContent={"column"}
+            style={{ marginTop: "86px", marginLeft: "24px" }}
+          >
+            <Box>
+              <img src="/icons/welcome.svg" alt="welcome" />
+            </Box>
+            <Box className="define_restaurant">
+              The Authentic Restaurant & Cafe
+            </Box>
+            <Box className="timeline_service">Open 24/7</Box>
+            <Box sx={{ mt: "90px" }}>
+              <Button
+                variant="contained"
+                style={{
+                  width: "180px",
+                  height: "60px",
+                  background: "#1976d2",
+                  color: "#ffffff",
+                }}
+              >
+                Sign up
+              </Button>
+            </Box>
+          </Stack>
+
+          <Stack flexDirection={"column"}>
+            <Box className="big_img"></Box>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
+}
