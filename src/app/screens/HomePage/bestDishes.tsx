e06cb8ccbec48c479a161eab9cfbeb3ec41e0f9e
ALEKSEY0305 <alex.romanovich.ak@gmail.com>
2023-02-25 05:46:33 +0900

    feat: ads homepage publishing

--- src/app/screens/HomePage/bestDishes.tsx
+++ src/app/screens/HomePage/bestDishes.tsx
@@ -1,10 +1,98 @@
-import { Container } from "@mui/material";
+
+import { Box, Container, Stack } from "@mui/material";
 import React from "react";
 
 export function BestDishes() {
   return (
-    <div className="top_restaurant_frame">
-      <Container>BestDishes</Container>
+    <div className="best_dishes_frame">
+      <Container>
+        <Stack flexDirection={"column"} alignItems={"center"}>
+          <Box className="category_title">Recommended Dishes</Box>
+          <Stack sx={{ mt: "43px" }} flexDirection={"row"}>
+            
+            <Box className="dish_box">
+              <Stack className="dish_img">
+                <div className="dish_sale">normal size</div>
+                <div className="view_btn">
+                  See more
+                  <img
+                    src="/icons/arrow.svg"
+                    alt="arrow"
+                    style={{ marginLeft: "9px" }}
+                  />
+                </div>
+              </Stack>
+              <Stack className="dish_desc">
+                <span className="dish_title_text">Ramen</span>
+                <span className="dish_desc_text">
+                  <span className="price"> $11 </span>
+                </span>
+              </Stack>
+            </Box>
+
+            <Box className="dish_box">
+              <Stack className="dish_img">
+                <div className="dish_sale">normal size</div>
+                <div className="view_btn">
+                  See more
+                  <img
+                    src="/icons/arrow.svg"
+                    alt="arrow"
+                    style={{ marginLeft: "9px" }}
+                  />
+                </div>
+              </Stack>
+              <Stack className="dish_desc">
+                <span className="dish_title_text">Ramen</span>
+                <span className="dish_desc_text">
+                  <span className="price"> $11 </span>
+                </span>
+              </Stack>
+            </Box>
+
+            <Box className="dish_box">
+              <Stack className="dish_img">
+                <div className="dish_sale">normal size</div>
+                <div className="view_btn">
+                  See more
+                  <img
+                    src="/icons/arrow.svg"
+                    alt="arrow"
+                    style={{ marginLeft: "9px" }}
+                  />
+                </div>
+              </Stack>
+              <Stack className="dish_desc">
+                <span className="dish_title_text">Ramen</span>
+                <span className="dish_desc_text">
+                  <span className="price"> $11 </span>
+                </span>
+              </Stack>
+            </Box>
+            
+            <Box className="dish_box">
+              <Stack className="dish_img">
+                <div className="dish_sale">normal size</div>
+                <div className="view_btn">
+                  See more
+                  <img
+                    src="/icons/arrow.svg"
+                    alt="arrow"
+                    style={{ marginLeft: "9px" }}
+                  />
+                </div>
+              </Stack>
+              <Stack className="dish_desc">
+                <span className="dish_title_text">Ramen</span>
+                <span className="dish_desc_text">
+                  <span className="price"> $11 </span>
+                </span>
+              </Stack>
+            </Box>
+
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
