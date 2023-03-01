db9d1e865b2bda31dc97d0bd89f20d4db2d18e46
ALEKSEY0305 <alex.romanovich.ak@gmail.com>
2023-03-02 04:40:29 +0900

    feat: community screen develop

---
+++ src/app/screens/CommunityPage/communityChats.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { Avatar, Box, Stack } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+
+export function CommunityChats() {
+  /** INITIALIZATIONS **/
+  const [messagesList, setMessagesList] = useState([]);
+
+  return (
+    <Stack className={"chat_frame"}>
+      <Box className={"chat_top"}>Live Chat</Box>
+      <Box className={"chat_content"}>
+        <Stack className={"chat_main"}>
+          <Box
+            flexDirection={"row"}
+            style={{ display: "flex" }}
+            sx={{ m: "10px 0px" }}
+          >
+            <div className={"msg_left"}>Welcome to Live Chat!🥳</div>
+          </Box>
+          <Box
+            flexDirection={"row"}
+            style={{ display: "flex" }}
+            alignItems={"flex-end"}
+            justifyContent={"flex-end"}
+            sx={{ m: "10px 0px" }}
+          >
+            <div className={"msg_right"}>Write a message to other users👨‍💻</div>
+          </Box>
+          <Box
+            flexDirection={"row"}
+            style={{ display: "flex" }}
+            sx={{ m: "10px 0px" }}
+          >
+            <Avatar alt={"martin"} src={"/community/cute_girl.jpg"} />
+            <div className={"msg_left"}>Hey! Nice community chat!😇</div>
+          </Box>
+          <Box
+            flexDirection={"row"}
+            style={{ display: "flex" }}
+            alignItems={"flex-end"}
+            justifyContent={"flex-end"}
+            sx={{ m: "10px 0px" }}
+          >
+            <div className={"msg_right"}>Thanks😉</div>
+          </Box>
+        </Stack>
+      </Box>
+      <Box className={"chat_bott"}>
+        <input
+          type={"text"}
+          name={"message"}
+          className={"msg_input"}
+          placeholder={"Send a message"}
+        />
+        <button className={"send_msg_btn"}>
+          <SendIcon style={{ color: "#fff", cursor: "pointer" }} />
+        </button>
+      </Box>
+    </Stack>
+  );
+}
